fd19eafd422f3a738166e9aa2c263863c4ba5d27
Swaakk <swathykrishna.thayyil@gmail.com>
2022-03-10 22:18:00 +0530

    finished html and css started animation with javascripts

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+let container = document.querySelector(".container");
+let card = document.querySelector(".card");
+
+
+container.addEventListener("mousemove",(e)=>{
+    let xAxis = window.innerWidth - e.pageX
+    let yAxis = window.innerHeight - e.pageY
+
+    card.style.transform = `rotateX(${yAxis}deg) rotateY(${xAxis}deg)`
+})
